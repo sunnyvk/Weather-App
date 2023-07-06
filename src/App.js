3611e99bf791ed3212331c2b4d05e2964fb90b57
sunnyvk <khambayatsunny278@gmail.com>
2023-07-07 02:01:25 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "11836b517e2b6bcfdc78d4d625e577ce",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
